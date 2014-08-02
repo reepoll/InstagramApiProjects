61f1096fc2a99594919549dbf4618682de6b111a
reepoll <rappoli.chris@gmail.com>
2014-08-02 15:45:03 -0400

    updating APIs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //http://cfg.good.is/challenges/instagram-api 
 
 $(document).ready(function() {
-	var tag = "siboston";
-	var count = 4;
+	var tag = "southie";
+	var count = 5;
 	var client_id = '97437151d4c9456bb6c2b36a500e93b4';
 
 	function grabImages(){
@@ -32,19 +32,20 @@ $(document).ready(function() {
 		// instagram_data.meta is where the secret messages from Instagram live
   		// and instagram_data.meta.code holds the status code of the request
   		// 404 means nothing was found, and 200 means everything is all good so...
-  		alert('in onDataLoaded')
 		if(instagram_data.meta.code == 200) {
 			// create a variable that holds all returned payload
-			var photos = instagram_data.data;
+			var data = instagram_data.data;
 
 			//as long as that variable holds data (does not = ) then...
-			if(photos.length > 0){
+			if(data.length > 0){
 				//since there are multiple objects in the payload we have
       			//to create a loop
-      			for (var i = 0; i < photos.length; i++){
+      			for (var i = 0; i < data.length; i++){
       				//then we create and append to the DOM an  element in jQuery
         			//the source of which is the thumbnail of the photo
-        			$('#target').append('<img src="' + photos[i].images.standard_resolution.url + '">');
+        			$('#target').append('<img src="' + data[i].images.standard_resolution.url + '">');
+        			$('#user-pic').append('<img src="' + data[i].user.profile_picture + '">');
+        			$('#likes').append(data[i].likes.count + "-" + i + "/");
     			}
       		} else {
 					//if the photos variable doesn't hold data
